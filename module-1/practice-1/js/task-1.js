fd041c9cfac421e653d16c31c7b2485da6a6aa37
iKozyrniy <69641814+iKozyrniy@users.noreply.github.com>
2021-02-20 19:58:43 +0200

    Add files via upload

---
+++ module-1/practice-1/js/task-1.js
@@ -0,0 +1,32 @@
+let str = "Привіт";
+console.log (typeof str);
+
+let num = 123;
+console.log (typeof num);
+
+let flag = true;
+console.log (typeof flag);
+
+let txt = "true";
+console.log (typeof txt);
+
+let num2 = 12345;
+console.log (typeof numbers);
+
+const str2 = "Привіт";
+console.log (typeof str);
+
+const num3 = 123;
+console.log (typeof num);
+
+const flag2 = true;
+console.log (typeof flag);
+
+const txt2 = "true";
+console.log (typeof txt);
+
+const numbers4 = 12345;
+console.log (typeof numbers);
+
+
+
