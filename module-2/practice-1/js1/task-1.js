d7153593417b4b904ef5f4c7d21470580fdf99cc
iKozyrniy <69641814+iKozyrniy@users.noreply.github.com>
2021-03-18 17:38:53 +0200

    Add files via upload

---
+++ module-2/practice-1/js1/task-1.js
@@ -0,0 +1,7 @@
+const arr1 = [1, 5, '4', 'hello'];
+const arr2 = [true, 2, {}, ['a'], 222];
+
+console.log(arr1[2]);
+console.log(arr2[2]);
+
+console.log(arr1[2] + arr2[2]);
---
+++ module-2/practice-1/js1/task-2.js
@@ -0,0 +1,7 @@
+const arr1 = [1, 5, '4', 'hello'];
+
+arr1[4] = 22;
+
+for (const arr of arr1) {
+    console.log(arr);
+}
